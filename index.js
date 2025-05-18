f7f107bce23650b0325c480ee4eaaeb33d7d8544
Dr.sheriff <kiptoobethuel44@gmail.com>
2025-05-18 14:10:54 +0300

    clear button modified

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function appendToDisplay(value) {
     document.getElementById("display").value += value;
 
 }
-function clearToDisplay(){
-    document.getElementById("display").value="";
+function clearToDisplay() {
+    var displayValue = document.getElementById("display").value;
+    document.getElementById("display").value = displayValue.slice(0, -1);
 }
+
  function calculateResult() {
         try {
             document.getElementById("display").value = eval(document.getElementById("display").value);
